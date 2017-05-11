e10fbac61ec2b170f4d07cb2fc078b1863fa2370
lotfar <lutfurkayes@yahoo.com>
2017-05-11 15:16:05 +0200

    application

---
+++ application.js
@@ -0,0 +1,16 @@
+angular.module('showExplorer', ['ngMaterial','ngResource']);
+angular.module('showExplorer').constant('ApiUrl','http://www.omdbapi.com/');
+angular.module('showExplorer').constant('MonthNames',[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]);
